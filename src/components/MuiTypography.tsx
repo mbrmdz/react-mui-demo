632f55f85be161f6cf6d11374a10967f6bd4b671
Michelle Bermudez <michelleg.bermudez@gmail.com>
2024-04-15 22:33:43 +0800

    Initial commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,30 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4">h4 Heading</Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+
+      <Typography variant="subtitle1">Sub title 1</Typography>
+      <Typography variant="subtitle2">Sub title 2</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores esse
+        distinctio molestiae ad dolorem explicabo dignissimos sequi? Numquam
+        natus nesciunt quam quibusdam fuga ducimus reprehenderit illum eos quos,
+        incidunt maiores?
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa
+        perspiciatis voluptates laboriosam sequi modi nisi deleniti iusto
+        corporis, officia corrupti eveniet quo vitae? Quasi vero officia, enim
+        earum ut recusandae.
+      </Typography>
+    </div>
+  );
+};
