b306b72adf4dd122f3788b96ed91de2036cf80a8
Michelle Bermudez <michelleg.bermudez@gmail.com>
2024-04-29 08:48:05 +0800

    MuiButton tutorial

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -7,13 +7,22 @@ import {
   ToggleButton,
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
-
 import FormatBoldIcon from "@mui/icons-material/FormatBold";
 import FormatItalicIcon from "@mui/icons-material/FormatItalic";
 import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
 import { useState } from "react";
 
 export const MuiButton = () => {
+  const [formats, setFormats] = useState<string | null>(null);
+  console.log({
+    formats,
+  });
+  const handleFormatChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    updatedFormats: string | null
+  ) => {
+    setFormats(updatedFormats);
+  };
   return (
     <Stack spacing={4}>
       <Stack spacing={2} direction="row">
@@ -55,33 +64,55 @@ export const MuiButton = () => {
         </Button>
       </Stack>
       <Stack spacing={2} direction="row">
-        <Button variant="contained" startIcon={<SendIcon />}>
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => alert("clicked")}
+        >
           Send
         </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+        <IconButton aria-label="send" color="success" size="small">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+      <Stack direction="row">
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          size="small"
+          color="secondary"
+          aria-label="alignment button group"
+        >
+          <Button onClick={() => alert("Left clicked")}>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+      <Stack direction="row">
+        <ToggleButtonGroup
+          aria-label="text formatting"
+          value={formats}
+          onChange={handleFormatChange}
+          size="small"
+          color="success"
+          orientation="vertical"
+          exclusive
+        >
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-labe="itaic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value="underlined" aria-label="underlined">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
     </Stack>
-
-    // <Stack spacing={4}>
-    //   <Stack spacing={2} direction="row">
-    //     <Button variant="text">Text</Button>
-    //     <Button variant="contained">Contained</Button>
-    //     <Button variant="outlined">Outlined</Button>
-    //   </Stack>
-    //   <Stack direction="row">
-    //     <ToggleButtonGroup aria-label="text formatting">
-    //       <ToggleButton value="bold" aria-label="bold">
-    //         <FormatBoldIcon />
-    //       </ToggleButton>
-
-    //       <ToggleButton value="italic" aria-label="bold">
-    //         <FormatItalicIcon />
-    //       </ToggleButton>
-
-    //       <ToggleButton value="underlined" aria-label="bold">
-    //         <FormatUnderlinedIcon />
-    //       </ToggleButton>
-    //     </ToggleButtonGroup>
-    //   </Stack>
-    // </Stack>
   );
 };
